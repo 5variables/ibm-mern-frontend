8056a5eae00a178f4312b54ce1760418231f7431
notna4 <cristicanceal@gmail.com>
2023-07-10 17:23:53 +0300

    register login layout

--- src/pages/register/login.js
+++ src/pages/register/login.js
@@ -1,78 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-
+import LoginForm from '@/components/molecules/LoginForm';
+import './register-style.css';
 
 const LoginPage = () => {
     const router = useRouter();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleFormSubmit = async (e) => {
-        e.preventDefault();
-
-        // check if all fields are completed
-        if (email && password) {
-            
-            try {
-                const response = await axios.post('http://localhost:3001/register/login', {
-                    mail: email,
-                    password: password,
-                });
-            
-                // console.log(response.data.token); 
-                localStorage.setItem('token', response.data.token);
-                router.push('/');
-            } catch (error) {
-                console.error(error);
-            }
-
-            
-            setEmail('');
-            setPassword('');
-        } else {
-           
-            console.log('Please fill in all fields.');
-        }
-    };
-
     return(
-        <div>
-            <h1>Welcome back, login in your account</h1>
-            <div style={{ display: 'flex' }}>
-                <div style={{ flex: 3 }}>
-                    <form onSubmit={handleFormSubmit}>
-                    <div>
-                        <label>
-                        Email:
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                        Password:
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        </label>
-                    </div>
-                    <div>
-                        <button type="submit">Login</button>
-                    </div>
-                    <div>
-                        <h3 onClick={() => {
-                            router.push('/register/new-user');
-                        }}><u>I don't have an account. Click here</u></h3>
-                    </div>
-                    </form>
-                </div>
+        <div className='main'>
+            <div className='left'>
+                <LoginForm />
+            </div>
+            <div className='right'>
+                
             </div>
         </div>
     );
