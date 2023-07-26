b5c1c22445ebda17759548053c1de02d54253e54
Laes Oana-Adina <adinalaes@gmail.com>
2023-07-26 18:39:35 +0300

    Added field validation for login and register forms

--- src/components/molecules/LoginForm.js
+++ src/components/molecules/LoginForm.js
@@ -1,60 +1,78 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import Input from '@/components/atoms/Input';
 import LargeButton from '@/components/atoms/LargeButton';
 import './molecules-style.css';
 
-
 const RegisterForm = () => {
     const router = useRouter();
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
 
-        // check if all fields are completed
+        // Reset previous error messages
+        setEmailError('');
+        setPasswordError('');
+
+
         if (email && password) {
-            
             try {
                 const response = await axios.post('http://localhost:3001/register/login', {
                     mail: email,
                     password: password,
                 });
-            
-                // console.log(response.data.token); 
+
+
                 localStorage.setItem('token', response.data.token);
                 router.push('/');
             } catch (error) {
                 console.error(error);
+ 
+                setPasswordError('Incorrect email or password');
             }
 
-            
             setEmail('');
             setPassword('');
         } else {
-           
-            console.log('Please fill in all fields.');
+            // Validation for empty fields
+            if (!email) {
+                setEmailError('Please enter your email.');
+            }
+            if (!password) {
+                setPasswordError('Please enter your password.');
+            }
         }
     };
 
-    return(
+    return (
         <div className="register-form">
             <h1>Welcome back!</h1>
             <form onSubmit={handleFormSubmit}>
-            <div className="mid-fields">
-                <Input _onInputChange={(value) => setEmail(value)} _placeholder={"Mail"}/>
-                <Input _onInputChange={(value) => setPassword(value)} _placeholder={"Password"}/>
-            </div>
+                <div className="mid-fields">
+                    <Input
+                        _onInputChange={(value) => setEmail(value)}
+                        _placeholder={"Mail"}
+                    />
+                    {emailError && <p className="error-message">{emailError}</p>}
+                    <Input
+                        _onInputChange={(value) => setPassword(value)}
+                        _placeholder={"Password"}
+                    />
+                    {passwordError && <p className="error-message">{passwordError}</p>}
+                </div>
 
-            <div className="bottom-fields">
-                <LargeButton _label="Sign In -->" _type="submit"/>
-                <h3 onClick={() => {
-                    router.push('/register/new-user');
-                }}><u>I don't have an account {"->"}</u></h3>
-            </div>
+                <div className="bottom-fields">
+                    <LargeButton _label="Sign In -->" _type="submit" />
+                    <h3 onClick={() => {
+                        router.push('/register/new-user');
+                    }}><u>I don't have an account {"->"}</u></h3>
+                </div>
             </form>
         </div>
     );
